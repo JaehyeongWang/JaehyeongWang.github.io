32de3be080bffaf86eeccb4d8f72b4b9152f5cfe
JaehyeongWang <jahyeng224@g.skku.edu>
2023-05-23 14:54:39 +0900

    test test.py again1

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ def fib(n: int) -> int:
     return n if n < 2 else fib(n-1) + fib(n-2)
 
 def test_fibonacci():
-    assert fib(int(os.environ["FIB_INPUT"])) == 55, "false!"
+    return fib(int(os.environ["FIB_INPUT"])) == 55
 
 print(test_fibonacci())
