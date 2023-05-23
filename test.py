5641bcfe8b964e554f24b8d1d5d50288f3311c1b
JaehyeongWang <jahyeng224@g.skku.edu>
2023-05-23 21:42:41 +0900

    fork from arifszn

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def fib(n: int) -> int:
-    return n if n < 2 else fib(n-1) + fib(n-2)
-
-def test_fibonacci():
-    return fib(int(os.environ["FIB_INPUT"])) == 55
-
-print(test_fibonacci())
